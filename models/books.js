650b5dee198fdc870b9ccd342ba38e9b923c1edf
muhammadsyihab <muhammadsyihab406@gmail.com>
2024-06-02 23:12:46 +0800

    edit json wiki.url form books

--- models/books.js
+++ models/books.js
@@ -1,158 +1,160 @@
 'use strict';
 
+
+
 module.exports = [
     {
         "id" : "1",
         "title": "Dune",
         "year": "1965",
         "author": "Frank Herbert",
-        "wiki_url": "https://en.wikipedia.org/wiki/Dune_(novel)"
+        "wiki_url": "https://picsum.photos/200"
     },
     {
         "id" : "2",
         "title": "Dune Messiah",
         "year": "1969",
         "author": "Frank Herbert",
-        "wiki_url": "https://en.wikipedia.org/wiki/Dune_Messiah"
+        "wiki_url": "https://picsum.photos/201"
     },
     {
         "id" : "3",
         "title": "Children of Dune ",
         "year": "1976",
         "author": "Frank Herbert",
-        "wiki_url": "https://en.wikipedia.org/wiki/Children_of_Dune"
+        "wiki_url": "https://picsum.photos/202"
     },
     {
         "id" : "4",
         "title": "God Emperor of Dune",
         "year": "1981",
         "author": "Frank Herbert",
-        "wiki_url": "https://en.wikipedia.org/wiki/God_Emperor_of_Dune"
+        "wiki_url": "https://picsum.photos/203"
     },
     {
         "id" : "5",
         "title": "Heretics of Dune",
         "year": "1984",
         "author": "Frank Herbert",
-        "wiki_url": "https://en.wikipedia.org/wiki/Heretics_of_Dune"
+        "wiki_url": "https://picsum.photos/204"
     },
     {
         "id" : "6",
         "title": "Chapterhouse: Dune",
         "year": "1985",
         "author": "Frank Herbert",
-        "wiki_url": "https://en.wikipedia.org/wiki/Chapterhouse:_Dune"
+        "wiki_url": "https://picsum.photos/205"
     },
     {
         "id" : "7",
         "title": "House Atreides",
         "year": "1999",
         "author": ["Brian Herbert", "Kevin J. Anderson"],
-        "wiki_url": "https://en.wikipedia.org/wiki/Dune:_House_Atreides"
+        "wiki_url": "https://picsum.photos/206"
     },
     {
         "id" : "8",
         "title": "House Harkonnen",
         "year": "2000",
         "author": ["Brian Herbert", "Kevin J. Anderson"],
-        "wiki_url": "https://en.wikipedia.org/wiki/Dune:_House_Harkonnen"
+        "wiki_url": "https://picsum.photos/207"
     },
     {
         "id" : "9",
         "title": "House Corrino",
         "year": "2001",
         "author": ["Brian Herbert", "Kevin J. Anderson"],
-        "wiki_url": "https://en.wikipedia.org/wiki/Dune:_House_Corrino"
+        "wiki_url": "https://picsum.photos/208"
     },
     {
         "id" : "10",
         "title": "The Butlerian Jihad",
         "year": "2002",
         "author": ["Brian Herbert", "Kevin J. Anderson"],
-        "wiki_url": "https://en.wikipedia.org/wiki/Dune:_The_Butlerian_Jihad"
+        "wiki_url": "https://picsum.photos/209"
     },
     {
         "id" : "11",
         "title": "The Machine Crusade",
         "year": "2003",
         "author": ["Brian Herbert", "Kevin J. Anderson"],
-        "wiki_url": "https://en.wikipedia.org/wiki/Dune:_The_Machine_Crusade"
+        "wiki_url": "https://picsum.photos/210"
     },
     {
         "id" : "12",
         "title": "The Battle of Corrin",
         "year": "2004",
         "author": ["Brian Herbert", "Kevin J. Anderson"],
-        "wiki_url": "https://en.wikipedia.org/wiki/Dune:_The_Battle_of_Corrin"
+        "wiki_url": "https://picsum.photos/211"
     },
     {
         "id" : "13",
         "title": "Hunters of Dune ",
         "year": "2006",
         "author": ["Brian Herbert", "Kevin J. Anderson"],
-        "wiki_url": "https://en.wikipedia.org/wiki/Hunters_of_Dune"
+        "wiki_url": "https://picsum.photos/212"
     },
     {
         "id" : "14",
         "title": "Sandworms of Dune",
         "year": "2007",
         "author": ["Brian Herbert", "Kevin J. Anderson"],
-        "wiki_url": "https://en.wikipedia.org/wiki/Sandworms_of_Dune"
+        "wiki_url": "https://picsum.photos/213"
     },
     {
         "id" : "15",
         "title": "Paul of Dune",
         "year": "2008",
         "author": ["Brian Herbert", "Kevin J. Anderson"],
-        "wiki_url": "https://en.wikipedia.org/wiki/Paul_of_Dune"
+        "wiki_url": "https://picsum.photos/214"
     },
     {
         "id" : "16",
         "title": "The Winds of Dune",
         "year": "2009",
         "author": ["Brian Herbert", "Kevin J. Anderson"],
-        "wiki_url": "https://en.wikipedia.org/wiki/The_Winds_of_Dune"
+        "wiki_url": "https://picsum.photos/215"
     },
     {
         "id" : "17",
         "title": "Sisterhood of Dune",
         "year": "2012",
         "author": ["Brian Herbert", "Kevin J. Anderson"],
-        "wiki_url": "https://en.wikipedia.org/wiki/Sisterhood_of_Dune"
+        "wiki_url": "https://picsum.photos/216"
     },
     {
         "id" : "18",
         "title": "Mentats of Dune",
         "year": "2014",
         "author": ["Brian Herbert", "Kevin J. Anderson"],
-        "wiki_url": "https://en.wikipedia.org/wiki/Mentats_of_Dune"
+        "wiki_url": "https://picsum.photos/217"
     },
     {
         "id" : "19",
         "title": "Navigators of Dune",
         "year": "2016",
         "author": ["Brian Herbert", "Kevin J. Anderson"],
-        "wiki_url": "https://en.wikipedia.org/wiki/Navigators_of_Dune"
+        "wiki_url": "https://picsum.photos/218"
     },
     {
         "id" : "20",
         "title": "Dune: The Duke of Caladan",
         "year": "2020",
         "author": ["Brian Herbert", "Kevin J. Anderson"],
-        "wiki_url": "https://en.wikipedia.org/wiki/Dune:_The_Duke_of_Caladan"
+        "wiki_url": "https://picsum.photos/219"
     },
     {
         "id" : "21",
         "title": "Dune: The Lady of Caladan",
         "year": "2021",
         "author": ["Brian Herbert", "Kevin J. Anderson"],
-        "wiki_url": "https://en.wikipedia.org/wiki/Dune:_The_Lady_of_Caladan"
+        "wiki_url": "https://picsum.photos/220"
     },
     {
         "id" : "22",
         "title": "Dune: The Heir of Caladan",
         "year": "TBA",
         "author": ["Brian Herbert", "Kevin J. Anderson"],
-        "wiki_url": "https://en.wikipedia.org/wiki/Dune:_The_Heir_of_Caladan"
+        "wiki_url": "https://picsum.photos/221"
     },
 ]
